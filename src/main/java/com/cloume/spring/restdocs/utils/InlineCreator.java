2be1ba7e16e74daac10f6a353199557fef48c6e9
SUGE2016 <gxu@cloume.com>
2017-08-13 17:17:35 +0800

    增加create重载

--- src/main/java/com/cloume/spring/restdocs/utils/InlineCreator.java
+++ src/main/java/com/cloume/spring/restdocs/utils/InlineCreator.java
@@ -1,10 +1,16 @@
 package com.cloume.spring.restdocs.utils;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class InlineCreator {
 
 	static public <T> T create(T emptyBean, Function<T, T> initializer) {
 		return initializer.apply(emptyBean);
+	}
+	
+	static public <T> T create(T emptyBean, Consumer<T> initializer) {
+		initializer.accept(emptyBean);
+		return emptyBean;
 	}
 }
